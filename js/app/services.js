1b951da448c015b84c92ebe61264f36c344bb445
Maodo DIOP <dmaodo@gmail.com>
2016-08-03 10:50:22 +0000

    Suppression de fichiers inutiles

--- js/app/services.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('todoService', ['ngResource']).
-        factory('Todo', function ($resource) {
-            return $resource('rest/todo/:id', {}, {
-                'save': {method:'PUT'}
-            });
-        });
